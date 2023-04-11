a8d7ac10ee12c13c06044d9d9ab4a15001c84bda
EricLionDev <ericfreisleben@gmail.com>
2023-04-11 01:53:19 -0400

    finished layout
    added some basic js functionality

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ let formContainer = document.getElementById("form-container");
 bgBlur.style.display = "none";
 formContainer.style.display = "none";
 
-let myLibrary = [];
+let myCharacters = [];
 
-function Character() {}
+function Character(name, camp, gender, charclass, race, level, active, desc) {
+  this.name = "";
+  this.camp = "";
+  this.gender = "";
+  this.charclass = "";
+  this.race = "";
+  this.level = "";
+  this.active = "";
+  this.desc = "";
+}
 
-function addCharToLib() {
+function newChar() {
   // do stuff here
 }
 
@@ -18,5 +27,21 @@ function showForm() {
   } else {
     formContainer.style.display = "none";
     bgBlur.style.display = "none";
+  }
+}
+
+// ACTIVE BUTTON
+let activeButton = document.getElementById("list-char-active");
+
+function swapActive() {
+  console.log("click");
+  if (activeButton.classList.contains("is-active")) {
+    activeButton.classList.add("not-active");
+    activeButton.classList.remove("is-active");
+    activeButton.innerText = "Not Active";
+  } else {
+    activeButton.classList.add("is-active");
+    activeButton.classList.remove("not-active");
+    activeButton.innerText = "Active";
   }
 }
